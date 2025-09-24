fbf198a998345a75b576d852b2b1e5bc447abdaf
Dindarfitri <dindaarfitri0110@gmail.com>
2025-09-24 20:27:06 +0700

    First commit - ShoppingList app

--- app/src/main/java/com/example/shoppinglist/ui/theme/Color.kt
+++ app/src/main/java/com/example/shoppinglist/ui/theme/Color.kt
@@ -2,10 +2,34 @@ package com.example.shoppinglist.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
+// Light Theme
+val BluePrimary = Color(0xFF1976D2)         // Biru utama
+val BlueOnPrimary = Color(0xFFFFFFFF)
+val BluePrimaryContainer = Color(0xFFBBDEFB)
+val BlueOnPrimaryContainer = Color(0xFF0D47A1)
 
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
+val BlueSecondary = Color(0xFF03A9F4)
+val BlueOnSecondary = Color(0xFF000000)
+val BlueSecondaryContainer = Color(0xFFB3E5FC)
+val BlueOnSecondaryContainer = Color(0xFF01579B)
+
+val BlueBackground = Color(0xFFE3F2FD)
+val BlueOnBackground = Color(0xFF000000)
+val BlueSurface = Color(0xFFFFFFFF)
+val BlueOnSurface = Color(0xFF000000)
+
+// Dark Theme
+val DarkBluePrimary = Color(0xFF0D47A1)
+val DarkBlueOnPrimary = Color(0xFFFFFFFF)
+val DarkBluePrimaryContainer = Color(0xFF1565C0)
+val DarkBlueOnPrimaryContainer = Color(0xFFBBDEFB)
+
+val DarkBlueSecondary = Color(0xFF0288D1)
+val DarkBlueOnSecondary = Color(0xFFFFFFFF)
+val DarkBlueSecondaryContainer = Color(0xFF0277BD)
+val DarkBlueOnSecondaryContainer = Color(0xFFB3E5FC)
+
+val DarkBlueBackground = Color(0xFF121212)
+val DarkBlueOnBackground = Color(0xFFE3F2FD)
+val DarkBlueSurface = Color(0xFF1E1E1E)
+val DarkBlueOnSurface = Color(0xFFE3F2FD)
